3a6fefcd982c2b6a9ffdd1d5cdb08a26018b6ace
Juliana Mashon <j.mariewise@gmail.com>
2024-06-06 16:30:27 -0700

    Added functions to control FarmBot via message broker and made changes to the following functions: get_token, get_info, edit_info, new_log (now new_log_API)

--- sidecar_functions.py
+++ sidecar_functions.py
@@ -1,71 +1,144 @@
 import json
 import requests
-from getpass import getpass # required for get_token
+from getpass import getpass # required for authorization token
+import paho.mqtt.publish as publish # required for message broker
 
-"""
-FUNCTION:       get_token
-PARAMETERS:     'EMAIL' is the user's email address associated w/ web app account, passed as a string
-                'PASSWORD' is the user's password associated w/ web app account, passed as a string
-"""
+RPC_REQUEST = {
+    "kind": "rpc_request",
+    "args": {
+        "label": ""
+    }
+}
 
-def get_token(EMAIL, PASSWORD):
+def get_token(EMAIL, PASSWORD, SERVER='https://my.farm.bot'):
     headers = {'content-type': 'application/json'}
     user = {'user': {'email': EMAIL, 'password': PASSWORD}}
-    response = requests.post(f'{'https://my.farm.bot'}/api/tokens', headers=headers, json=user)
+    response = requests.post(f'{SERVER}/api/tokens', headers=headers, json=user)
     return response.json()
 
-# get_token('email', 'password')
+def publish_single(PAYLOAD, TOKEN):
+    publish.single(
+        f'bot/{TOKEN['token']['unencoded']['bot']}/from_clients',
+        payload=json.dumps(PAYLOAD),
+        hostname=TOKEN['token']['unencoded']['mqtt'],
+        auth={
+            'username': TOKEN['token']['unencoded']['bot'],
+            'password': TOKEN['token']['encoded']
+        }
+    )
 
-"""
-FUNCTION:       get_info
-PARAMETERS:     'SOURCE' may be 'device', 'logs', 'peripherals', etc..., passed as a string
-                'TOKEN' is the user's auth token saved as a variable, passed as a variable
-"""
+def e_stop(TOKEN):
+    e_stop_message = {
+        **RPC_REQUEST,
+        "body": [{
+            "kind": "emergency_lock",
+            "args": {}
+        }]
+    }
 
-def get_info(SOURCE, TOKEN):
-    url = f'https:{TOKEN['token']['unencoded']['iss']}/api/'+SOURCE
+    publish_single(e_stop_message, TOKEN)
+
+def unlock(TOKEN):
+    unlock_message = {
+        **RPC_REQUEST,
+        "body": [{
+            "kind": "emergency_unlock",
+            "args": {}
+        }]
+    }
+
+    publish_single(unlock_message, TOKEN)
+
+def get_info(TOKEN, SOURCE, ID=''):
+    url = f'https:{TOKEN['token']['unencoded']['iss']}/api/'+SOURCE+'/'+ID
     headers = {'authorization': TOKEN['token']['encoded'], 'content-type': 'application/json'}
     response = requests.get(url, headers=headers)
-    return print(json.dumps(response.json(), indent=2))
+    return json.dumps(response.json(), indent=2)
 
-# get_info('device', TOKEN)
-
-"""
-FUNCTION:       edit_info
-PARAMETERS:     'SOURCE' may be 'device', 'logs', 'peripherals', etc..., passed as a string
-                'VALUE' may be the field you would like to change, passed as a string
-                'CHANGE' may be the new value of the field, passed as a string
-                'TOKEN' is the user's auth token saved as a variable, passed as a variable
-"""
-
-def edit_info(SOURCE, VALUE, CHANGE, TOKEN):
+def edit_info(TOKEN, SOURCE, VALUE, CHANGE, ID=''):
     new_value = {
         VALUE: CHANGE
     }
 
-    url = f'https:{TOKEN['token']['unencoded']['iss']}/api/'+SOURCE
+    url = f'https:{TOKEN['token']['unencoded']['iss']}/api/'+SOURCE+'/'+ID
     headers = {'authorization': TOKEN['token']['encoded'], 'content-type': 'application/json'}
-   
     response = requests.patch(url, headers=headers, data=json.dumps(new_value))
+    return json.dumps(response.json(), indent=2)
 
-    return print(json.dumps(response.json(), indent=2))
-
-# edit_info('device', 'name', 'Orange Orangutan', TOKEN)
-
-"""
-FUNCTION:       new_log
-PARAMETERS:     'MESSAGE' is the log message the user wishes to send, passed as a string
-                'TOKEN' is the user's auth token saved as a variable, passed as a variable
-"""
-
-def new_log(MESSAGE, TOKEN):
+def new_log_API(MESSAGE, CHANNEL, TYPE, VERBOSITY, TOKEN):
     new_message = {
-        'message': MESSAGE
+        'message': MESSAGE,
+        'channel': [CHANNEL], # Doesn't currently do anything
+        'type': TYPE,
+        'verbosity': VERBOSITY
     }
 
     url = f'https:{TOKEN['token']['unencoded']['iss']}/api/logs'
     headers = {'authorization': TOKEN['token']['encoded'], 'content-type': 'application/json'}
     response = requests.post(url, headers=headers, data=json.dumps(new_message))
-    return print(json.dumps(response.json(), indent=2))
+    return json.dumps(response.json(), indent=2)
 
-# new_log('This is a new log message', TOKEN)
+def new_log_BROKER(MESSAGE, TYPE, TOKEN):
+    new_message = {
+        **RPC_REQUEST,
+        "body": [{
+            'kind': 'send_message',
+            'args': {
+                'message': MESSAGE,
+                'message_type': TYPE
+            }
+        }]
+    }
+
+    publish_single(new_message, TOKEN)
+
+def move_to(X, Y, Z, TOKEN):
+    def axis_overwrite(AXIS, VALUE):
+        return {
+            "kind": "axis_overwrite",
+            "args": {
+                "axis": AXIS,
+                "axis_operand": {
+                    "kind": "numeric",
+                    "args": {
+                        "number": VALUE
+                    }
+                }
+            }
+        }
+
+    coordinates = {
+        **RPC_REQUEST,
+        "body": [{
+            "kind": "move",
+            "args": {},
+            "body": [
+                axis_overwrite("x", X),
+                axis_overwrite("y", Y),
+                axis_overwrite("z", Z)
+            ]
+        }]
+    }
+
+    publish_single(coordinates, TOKEN)
+
+def control_peripheral(TOKEN, ID, VALUE, TYPE):
+    control_message = {
+        **RPC_REQUEST,
+        "body": [{
+            "kind": "write_pin",
+            "args": {
+                "pin_value": VALUE, # Controls on/off or slider value
+                "pin_mode": TYPE, # Controls digital or analog
+                "pin_number": {
+                    "kind": "named_pin",
+                    "args": {
+                        "pin_type": "Peripheral",
+                        "pin_id": ID
+                    }
+                }
+            }
+        }]
+    }
+
+    publish_single(control_message, TOKEN)
