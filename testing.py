a4b018d96ac184df68d4df3297dc1c84983db426
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2024-07-25 10:45:35 -0700

    [tests] re-add removed tests

--- testing.py
+++ testing.py
@@ -1,3 +1,7 @@
+"""
+Farmbot Unit Tests
+"""
+
 import unittest
 import json
 from unittest.mock import Mock, patch
@@ -49,53 +53,94 @@ class TestFarmbot(unittest.TestCase):
     def test_get_token_bad_email(self, mock_post):
         """NEGATIVE TEST: function called with incorrect email"""
         mock_response = Mock()
-        error_response = {'error': 'bad email or password'}
-        mock_response.json.return_value = error_response
         mock_response.status_code = 422
         mock_post.return_value = mock_response
         fb = Farmbot()
-        with self.assertRaises(Exception):
-            fb.get_token('bad_email@gmail.com', 'test_pass_123')
+        # Call with bad email
+        fb.get_token('bad_email@gmail.com', 'test_pass_123')
         mock_post.assert_called_once_with(
             'https://my.farm.bot/api/tokens',
             headers={'content-type': 'application/json'},
             json={'user': {'email': 'bad_email@gmail.com', 'password': 'test_pass_123'}}
         )
+        # self.assertEqual(fb.error, 'ERROR: Incorrect email address or password.')
+        self.assertIsNone(fb.token)
         self.assertEqual(mock_post.return_value.status_code, 422)
 
     @patch('requests.post')
     def test_get_token_bad_server(self, mock_post):
         """NEGATIVE TEST: function called with incorrect server"""
         mock_response = Mock()
-        error_response = {'error': 'bad server'}
-        mock_response.json.return_value = error_response
         mock_response.status_code = 404
         mock_post.return_value = mock_response
         fb = Farmbot()
-        with self.assertRaises(Exception):
-            fb.get_token('test_email@gmail.com', 'test_pass_123', 'https://bad.farm.bot')
+        # Call with bad server
+        fb.get_token('test_email@gmail.com', 'test_pass_123', 'https://bad.farm.bot')
         mock_post.assert_called_once_with(
             'https://bad.farm.bot/api/tokens',
             headers={'content-type': 'application/json'},
             json={'user': {'email': 'test_email@gmail.com', 'password': 'test_pass_123'}}
         )
+        # self.assertEqual(fb.error, 'ERROR: The server address does not exist.')
+        self.assertIsNone(fb.token)
         self.assertEqual(mock_post.return_value.status_code, 404)
+
+    @patch('requests.request')
+    def test_get_info_endpoint_only(self, mock_request):
+        """POSITIVE TEST: function called with endpoint only"""
+        mock_token = {
+            'token': {
+                'unencoded': {'iss': '//my.farm.bot'},
+                'encoded': 'encoded_token_value'
+            }
+        }
+        mock_response = Mock()
+        expected_response = {'device': 'info'}
+        mock_response.json.return_value = expected_response
+        mock_response.status_code = 200
+        mock_request.return_value = mock_response
+        fb = Farmbot()
+        fb.api.api_connect.token = mock_token
+        # Call with endpoint only
+        response = fb.get_info('device')
+        mock_request.assert_called_once_with(
+            'GET',
+            'https://my.farm.bot/api/device',
+            headers={
+                'authorization': 'encoded_token_value',
+                'content-type': 'application/json'
+            },
+            json=None,
+        )
+        self.assertEqual(response, expected_response)
+        self.assertEqual(mock_request.return_value.status_code, 200)
 
     @patch('requests.get')
     def test_get_info_with_id(self, mock_get):
         """POSITIVE TEST: function called with valid ID"""
+        mock_token = {
+            'token': {
+                'unencoded': {'iss': '//my.farm.bot'},
+                'encoded': 'encoded_token_value'
+            }
+        }
         mock_response = Mock()
-        expected_info = {'id': '12345', 'info': 'farmbot_info'}
-        mock_response.json.return_value = expected_info
+        expected_response = {'peripheral': 'info'}
+        mock_response.json.return_value = expected_response
         mock_response.status_code = 200
         mock_get.return_value = mock_response
         fb = Farmbot()
-        info = fb.get_info('12345')
+        fb.token = mock_token
+        # Call with specific ID
+        response = fb.get_info('peripherals', '12345')
         mock_get.assert_called_once_with(
-            'https://my.farm.bot/api/resources/12345',
-            headers = {'authorization': self.token['token']['encoded'], 'content-type': 'application/json'}
+            'https://my.farm.bot/api/peripherals/12345',
+            headers = {
+                'authorization': 'encoded_token_value',
+                'content-type': 'application/json',
+            }
         )
-        self.assertEqual(info, expected_info)
+        self.assertEqual(response, json.dumps(expected_response, indent=2))
         self.assertEqual(mock_get.return_value.status_code, 200)
 
 if __name__ == '__main__':
