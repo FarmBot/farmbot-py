977ce0252c00d09dcfd1ad6732a2babdb2a8f29f
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2024-09-11 16:05:10 -0700

    move dev docs to contributing

--- farmbot_sidecar_starter_pack/main.py
+++ farmbot_sidecar_starter_pack/main.py
@@ -15,7 +15,7 @@ from .functions.peripherals import Peripherals
 from .functions.resources import Resources
 from .functions.tools import ToolControls
 
-VERSION = "1.6.0"
+VERSION = "1.6.1"
 
 
 class Farmbot():
