e8b3c892508988b3dfc7ae019e07c859bb77791f
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2024-12-03 14:54:35 -0800

    v2.0.7

--- farmbot/main.py
+++ farmbot/main.py
@@ -15,7 +15,7 @@ from .functions.peripherals import Peripherals
 from .functions.resources import Resources
 from .functions.tools import ToolControls
 
-VERSION = "2.0.6"
+VERSION = "2.0.7"
 
 
 class Farmbot():
